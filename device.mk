841efc2060742f098cb7475aab41d6d687d130e9
Derek <nielson925@gmail.com>
2022-06-22 22:46:29 -0600

    Build radio config 1.0
    Change-Id: I53dcf14333779718570eacc69d98a322a2af971b

--- device.mk
+++ device.mk
@@ -219,7 +219,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.0 \
-    android.hardware.radio.deprecated@1.0
+    android.hardware.radio.deprecated@1.0 \
+    android.hardware.radio.config@1.0-service
 
 # SEC config
 PRODUCT_COPY_FILES += \
