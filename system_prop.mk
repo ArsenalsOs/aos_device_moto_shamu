99cd9b5c03f6bd567ae42768fc4029497bd978d5
David <davidmolinamorales1999@gmail.com>
2022-10-12 21:17:16 -0600

    Fix bluetooth audio / pair
    Change-Id: I7867ac6806f9b4f69d0ad78f7d7158f845065f53

--- system_prop.mk
+++ system_prop.mk
@@ -28,7 +28,22 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    bluetooth.enable_timeout_ms=12000
+    bluetooth.enable_timeout_ms=12000 \
+    bluetooth.device.class_of_device=90,2,12 \
+    bluetooth.profile.a2dp.source.enabled=true \
+    bluetooth.profile.asha.central.enabled=true \
+    bluetooth.profile.avrcp.target.enabled=true \
+    bluetooth.profile.bas.client.enabled=true \
+    bluetooth.profile.gatt.enabled=true \
+    bluetooth.profile.hfp.ag.enabled=true \
+    bluetooth.profile.hid.device.enabled=true \
+    bluetooth.profile.hid.host.enabled=true \
+    bluetooth.profile.map.server.enabled=true \
+    bluetooth.profile.opp.enabled=true \
+    bluetooth.profile.pan.nap.enabled=true \
+    bluetooth.profile.pan.panu.enabled=true \
+    bluetooth.profile.pbap.server.enabled=true \
+    bluetooth.profile.sap.server.enabled=true
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
